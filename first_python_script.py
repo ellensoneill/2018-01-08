df074fb002f20b7741261e6edf6af9e22492496c
Ellen O'Neill <ellenoneill@kubrickgroup.com>
2019-01-08 09:17:22 +0000

    staged first_python_script

---
+++ first_python_script.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+'''My Module'''
+
+
+import numpy as np
+import pandas as pd
+import json
+
+
+def main():
+	'''
+	Author: EllenI
+	Date: 2019-01-08
+	Purpose:I am learning variables this is my main function
+	--
+	Amended On: 2019-04-01
+	Amended By: Dave
+	Issue Ticket No.: 78512
+	'''
+	myname = 'Ellen'
+	print(myname)
+
+# execute my main function	
+main()
